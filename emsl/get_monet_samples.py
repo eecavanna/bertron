0e1426f666ee7318e22e94939f0fdbc5f25735f8
Makena Dettmann <makena.dettmann@pnnl.gov>
2025-02-25 14:49:36 -0800

    Update to non-dev url

--- emsl/get_monet_samples.py
+++ emsl/get_monet_samples.py
@@ -1,6 +1,6 @@
 import requests
 import json
-res = requests.get("https://sc-data-dev.emsl.pnl.gov/ber/geofence?lat=46.34758&lon=-119.2779&fence=100000")
+res = requests.get("https://sc-data.emsl.pnnl.gov/ber/geofence?lat=46.34758&lon=-119.2779&fence=100000")
 with open("latlon_project_ids.json", 'w') as f:
     json_res = json.loads(res.text)
     f.write(json.dumps(json_res, indent=4))
